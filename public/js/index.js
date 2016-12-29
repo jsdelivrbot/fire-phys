69097e401e992645665c83459bc7ebaab0a53b9c
Kevin Cameron <kevinacameron@hotmail.com>
2016-12-29 01:04:24 -0800

    Render with Mithril

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,10 @@
 const M = Matter,
 
     engine = M.Engine.create(),
-    render = M.Render.create({
-        element : document.body,
-        engine
-    }),
+    // render = M.Render.create({
+    //     element : document.body,
+    //     engine
+    // }),
 
     bodies = [],
 
@@ -15,16 +15,17 @@ let idx = 0,
     now, last;
 
 engine.world.gravity.y = -1;
+// engine.world.gravity.x = -0.1;
 
-const s0 = M.Bodies.rectangle(400, 610, 810,  60, { isStatic : true }),
-    s1   = M.Bodies.rectangle(100, 330, 20,  500, { isStatic : true }),
-    s2   = M.Bodies.rectangle(700, 330, 20,  500, { isStatic : true }),
-    s3   = M.Bodies.rectangle(400, 330, 200, 500, { isStatic : true }),
-    base = M.Bodies.circle(400, 380, 150, { isStatic : true, friction : 0.1 });
+// const s0 = M.Bodies.rectangle(400, 610, 810,  60, { isStatic : true }),
+//     s1   = M.Bodies.rectangle(100, 330, 20,  500, { isStatic : true }),
+//     s2   = M.Bodies.rectangle(700, 330, 20,  500, { isStatic : true }),
+//     s3   = M.Bodies.rectangle(400, 330, 200, 500, { isStatic : true }),
+//     base = M.Bodies.circle(380, 380, 150, { isStatic : true, friction : 0, frictionStatic : 0 });
 
-M.World.add(engine.world, [ s0, s1, s2, base ]);
+// M.World.add(engine.world, [ s0, s1, s2, base ]);
 M.Engine.run(engine);
-M.Render.run(render);
+// M.Render.run( );
 
 function getRand(min, max) {
     return Math.random() * (max - min) + min;
@@ -33,34 +34,40 @@ function getRand(min, max) {
 function raf() {
     requestAnimationFrame(raf);
 
-    now = Date.now();
+    let x = getRand(130, 300),
+        y = getRand(500, 600),
+        r = getRand(1, 20);
 
-    if(!last || now - last > RATE) {
-        let num = getRand(2, 6);
-
-        last = now;
-
-        for(let i = 0; i < num; i++) {
-            let x = getRand(getRand(130, 200), getRand(630, 700)),
-                y = getRand(670, 500),
-                r = getRand(1, 20);
-
-            if(bodies[idx]) {
-                M.World.remove(engine.world, bodies[idx]);
-            }
-
-            bodies[idx] = M.Bodies.circle(x, y, r, {
-                collisionFilter : {
-                    group : -1
-                },
-                friction : 0.1
-                // force : { x : 10, y : 2 }
-            });
-            M.World.add(engine.world, [ bodies[idx] ]);
-
-            idx = idx === MAX ? 0 : idx + 1;
-        }
+    if(bodies[idx]) {
+        M.World.remove(engine.world, bodies[idx]);
     }
+
+    bodies[idx] = M.Bodies.circle(x, y, r, {
+        collisionFilter : {
+            group : -1
+        },
+        friction : 0,
+        frictionStatic : 0
+    });
+
+    M.Body.setVelocity(bodies[idx], { x : 5, y : 0 });
+    // M.Body.setDensity(bodies[idx], 0.001);
+    M.World.add(engine.world, [ bodies[idx] ]);
+
+    idx = idx === MAX ? 0 : idx + 1;
+
+    m.render(document.body, m("svg", {
+            width : 800,
+            height: 600
+        }, M.Composite.allBodies(engine.world)
+            // .filter((compositeBody) => compositeBody.circleRadius)
+            .map((compositeBody) => m("circle", {
+                cx : compositeBody.position.x,
+                cy : compositeBody.position.y,
+                r  : compositeBody.circleRadius
+            }))
+    ));
 }
 
 raf();
+
